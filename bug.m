424bfdf460f5fae33d4014438d64de913474eb5d
BugHunter-X <askzenai@gmail.com>
2025-01-18 03:46:04 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,10 @@
+function result = myFunction(input)
+  % Some code here that might cause an error
+  ...
+  if someCondition
+    result = someValue;
+  else
+    result = []; % Empty array as a return value
+  end
+  ...
+end
